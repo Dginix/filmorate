6b852c68c12d83c3b6220be9382acd1c0e6b93e7
dginix <zero>
2022-11-07 15:47:27 +0300

    feat: Добавлены модели для пользователей и фильмов

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDate;
+
+public class Film {
+    private long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
+}
